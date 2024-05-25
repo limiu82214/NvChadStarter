94fa38e177c75d67658bcf83db40fe3e6369cbfc
Nimo <limiu82214@gmail.com>
2024-05-26 01:27:22 +0800

    update

--- lua/configs/lspconfig_go.lua
+++ lua/configs/lspconfig_go.lua
@@ -1,11 +1,9 @@
 local capabilities = require("nvchad.configs.lspconfig").capabilities
-local on_init = require("nvchad.configs.lspconfig").on_init
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local lspconfig = require("lspconfig")
 local util = require "lspconfig/util"
 
 lspconfig.gopls.setup {
-  on_init = on_init,
   on_attach = on_attach,
   capabilities = capabilities,
 
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,31 +1,68 @@
-local plugins = {
-  "williamboman/mason.nvim",
-  opts = {
-    ensure_installed = {
-      "gopls",
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "gopls",
+        "delve",
+        "go-debug-adapter",
+        "gofumpt",
+        "goimports-reviser",
+        "golangci-lint",
+        "golines",
+        "gomodifytags",
+        "gotests",
+      },
     },
   },
   {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "comment",
+        "javascript",
+        "jsdoc",
+        "jsonc",
+        "lua",
+        "markdown",
+        "regex",
+        "yaml",
+        "go",
+        "git_config",
+        "git_rebase",
+        "gitattributes",
+        "gitignore",
+        "gomod",
+        "gosum",
+        "gowork",
+        "gotmpl",
+      },
+      highlight = {
+        enable = true,
+      },
+    },
+  },
+  {
+    -- go lsp
     "neovim/nvim-lspconfig",
     config = function()
-      require "configs.lspconfig"
       require "configs.lspconfig_go"
     end,
   },
   {
+    -- save to gofumpt
     "jose-elias-alvarez/null-ls.nvim",
     ft = "go",
     opts = function()
       return require "configs.null-ls"
     end,
   },
-  {
-    "mfussenegger/nvim-dap",
-    ft = "go",
-    opts = function()
-      return require "configs.null-ls"
-    end,
-  }
+--  {
+--    "mfussenegger/nvim-dap",
+--    ft = "go",
+--    opts = function()
+--      return require "configs.null-ls"
+--    end,
+--  }
 }
-
-return plugins
