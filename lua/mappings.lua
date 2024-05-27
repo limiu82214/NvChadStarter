7c2d43f4b1a5440bc6bc682b10cee112bba7e6cc
Nimo <limiu82214@gmail.com>
2024-05-27 18:25:13 +0800

    copy current path

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -25,3 +25,33 @@ map("n", "gT", ":bprevious<CR>")
 
 -- 恢復上一個搜尋
 map("n", "<leader>fr", ":Telescope resume<CR>")
+
+-- copy 相對路徑 的方法
+vim.api.nvim_create_user_command("CopyFileRelativePath", function()
+   -- 获取当前文件的绝对路径
+    local absolute_path = vim.fn.expand("%:p")
+    -- 获取当前工作目录
+    local cwd = vim.fn.getcwd()
+    -- 获取当前行号
+    local line_number = vim.fn.line('.')
+    -- 去掉工作目录部分，获取相对路径
+    local relative_path = absolute_path:sub(#cwd + 2)
+
+    vim.fn.setreg("+", relative_path .. ":" .. line_number)
+    vim.notify('Copied "' .. relative_path .. ":" .. line_number .. '" to the clipboard!')
+end, {}) 
+map("n", "<leader>cp", ":CopyFileRelativePath<CR>")
+
+-- copy 絕對路徑 的方法
+vim.api.nvim_create_user_command("CopyFileAbolutePath", function()
+   -- 获取当前文件的绝对路径
+    local absolute_path = vim.fn.expand("%:p")
+    local line_number = vim.fn.line('.')
+
+    vim.fn.setreg("+", absolute_path .. ":" .. line_number)
+    vim.notify('Copied "' .. absolute_path .. ":" .. line_number .. '" to the clipboard!')
+end, {}) 
+
+
+
+
