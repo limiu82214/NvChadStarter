837078ca8bd706041b9d506d4c5098b7845d04b4
Nimo <limiu82214@gmail.com>
2024-05-30 17:44:19 +0800

    update

--- lua/configs/lspconfig_go.lua
+++ lua/configs/lspconfig_go.lua
@@ -4,27 +4,27 @@ local lspconfig = require("lspconfig")
 local util = require "lspconfig/util"
 
 lspconfig.gopls.setup {
-  on_attach = function(client, bufnr)
-    on_attach(client, bufnr)
+    on_attach = function(client, bufnr)
+        on_attach(client, bufnr)
 
-    -- 可以用 gh 查看 滑鼠 hover 的訊息
-    local opts = { noremap = true, silent = true }
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gh', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+        -- 可以用 gh 查看 滑鼠 hover 的訊息
+        local opts = { noremap = true, silent = true }
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gh', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
 
-  end,
-  capabilities = capabilities,
+    end,
+    capabilities = capabilities,
 
-  cmd = {"gopls"},
-  filetypes = {"go", "gomod", "gowork", "gotmpl"},
-  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-  setting = {
-    gopls = {
-      completeUnimported = true,
-      usePlaceholders = true,
-      analyses = {
-        unusedparams = true,
-      },
+    cmd = {"gopls"},
+    filetypes = {"go", "gomod", "gowork", "gotmpl"},
+    root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+    setting = {
+        gopls = {
+            completeUnimported = true,
+            usePlaceholders = true,
+            analyses = {
+                unusedparams = true,
+            },
+        },
     },
-  },
 }
 
